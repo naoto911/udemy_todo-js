d6278f7df273f666482d9e71417eebedf9599a36
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2023-12-30 00:23:12 +0000

    テキストボックスの入力をliに反映する処理を作成

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // ｐ生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
